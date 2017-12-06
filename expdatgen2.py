934554e82b734099ec5b5cf60c07cbd336592051
Dan Stutts <stutts@mst.edu>
2017-12-06 08:22:49 -0600

    Added grapher2.py which plots y and z w.r.t. x, and more description in the headers of grapher.py and grapher2.py.  Also added the ability to read both tab and comma-delimited data files using re.split('\t|,',line) from the regular expressions module.

---
+++ expdatgen2.py
@@ -0,0 +1,77 @@
+""" 
+File: expdatgen.py
+Author: Dr. D. S. Stutts
+First release: 12-18-2015
+Revised 10-6-2017 to work with Python 3.6.2
+Generates sample data for exponential curve fit example.
+Based on expdatgen.m by D. S. Stutts: 2-4-2014
+"""
+
+# Import all of the necessary python libraries:
+from pylab import *
+import matplotlib.pyplot as plt
+import itertools# itteration tools for writedat() defined below
+import sys
+cur_version = sys.version_info
+
+# Create some empty lists to work with:
+x = []
+y = []
+xx = []
+yy = []
+
+# Create a numpy array of the independent variable x:
+x = linspace(-5,5, num=13)
+
+def F(x):
+    return 0.625+1.33*exp(-0.5*x);# y-vector
+       
+
+# Function to write formatted tab-delimited x,y data
+# From: https://rosettacode.org/wiki/Write_float_arrays_to_a_text_file#Python
+def writedat(filename, x, y, xprecision=8, yprecision=8):
+   with open(filename,'w') as f:
+       for a, b in zip(x, y):
+            print("%.*g\t%.*g" % (xprecision, a, yprecision, b), file=f)
+            # or, using the new-style formatting:
+            # print("{1:.{0}g}\t{3:.{2}g}".format(xprecision, 
+            # a, yprecision, b), file=f)  
+
+# Create array of function values for each x:
+y = F(x)
+
+# Write the data to data.txt:
+writedat("data.txt", x, y)
+
+infile = "data.txt"
+# Check to make sure the data was written correctly:
+try: 
+    data = open(infile, "r")# Get array out of input file
+except:
+    print ("Cannot find input file; Please try again.")
+    sys.exit(0)
+
+# Close the input file:
+close(infile)
+# Parse the x,y pairs into two lists:
+if cur_version[0]==3:# This is necesary due to the change in the type
+    # returned by map in Python3.x.x
+    for line in data: # Separate the x,y data by splitting at the white space (tab).
+        xx.append(list(map(float, (line).split()))[0])
+        yy.append(list(map(float, (line).split()))[1])
+
+if cur_version[0]==2:# This is necesary due to the change in the type
+    for line in data: # Separate the x,y data by splitting at the white space (tab).
+        xx.append(map(float, (line).split())[0])
+        yy.append(map(float, (line).split())[1])
+# Recast the data lists into numpy arrays for plotting:     
+xx = array(xx)
+yy = array(yy) 
+       
+# Plot the data:
+plt.plot(xx, yy, 'go', label='data')
+plt.title("Experimental Data")
+plt.xlabel('x')
+plt.ylabel('F(x)')
+grid(True) # Show a grid
+plt.show()# Show the plot
--- grapher2.py
+++ grapher2.py
@@ -1,5 +1,5 @@
 ####################--
-# File: grapher.py
+# File: grapher2.py
 # Plots x,y tab-delimited data
 # Author: D. S. Stutts
 # Associate Professor of Mechanical Engineering
@@ -8,25 +8,25 @@
 # Rolla, MO 65409-0050
 # Email: stutts@mst.edu
 # Original release: 9-5-2016
-# Language: Python 3.5.2
+# Language: Python 3.6.2
 ####################--
 
 """
-Graphs y with respect to x where
-x and y are tab or comma delimited numerical values
+Graphs y and x with respect to x where
+x,y, and z are tab or comma delimited numerical values
 in a text file.
 
-Example call: python grapher.py inputdatafile.txt
+Example call: python grapher2.py inputdatafile.txt
 
 The graph may be saved in PNG format, and the text
 may be redirected from stdout to a file like so:
 
-python grapher.py inputdatafile.txt > outdata.txt
+python grapher2.py inputdatafile.txt > outdata.txt
 
  # This code is copyrighted by the author, but released under the MIT
  # license:
 
-Copyright (c) 2016 -- grapher.py
+Copyright (c) 2016 -- grapher2.py
 
 S&T and the University of Missouri Board of Curators 
 license to you the right to use, modify, copy, and distribute this 
@@ -55,7 +55,7 @@ of this software include the following citation:
 
 @Misc{grapher_2016,
 author =   {Stutts, D. S.},
-title = {{grapher.py}: {Python data plotting script.}},
+title = {{grapher2.py}: {Python data plotting script.}},
 howpublished = {\\url{https://github.com/dsstutts/GRAPHER.git}},
 year = {2016}}
 
@@ -66,11 +66,12 @@ import sys
 from numpy import array
 import matplotlib.pyplot as plt
 import time #to allow time stamp on output
-import re # regular expressions
+import re
 # Test for Python version:
 cur_version = sys.version_info
 # Initialize some lists:
 linedat = []
+f = []
 x = []
 y = []
 xx = []
@@ -94,18 +95,26 @@ for line in data:
 data.seek(0) # Reset file pointer to the beginning
 
 linecount = 0
-  # read the 21st through total lines from the data file
-  # and fill x,y lists with floating point numbers:
+  # read the 1st through total lines from the data file
+  # and fill f, x,y lists with floating point numbers:
 if cur_version[0]==3:# This is necesary due to the change in the type
     for line in data:# returned by the map function in Python 3.x.x.
+        #linedat = list(map(float, line.re.split(' '|',',str)))
+        if linecount >= 1:
             linedat = list(map(float, re.split('\t|,',line)))
-            x.append(linedat[0])
-            y.append(linedat[1])
-            linecount += 1
+            f.append(linedat[0])
+            x.append(linedat[1])
+            y.append(linedat[2])
+        
+        linecount += 1
+
 else:
     for line in data:
-        x.append(map(float, re.split('\t|,',line))[0])
-        y.append(map(float, re.split('\t|,',line))[1])
+        if linecount >= 1:
+            linedat = re.split('\t|,',line)
+            f.append(linedat[0])
+            x.append(linedat[1])
+            y.append(linedat[2])
         linecount += 1
 data.close()
 xx = array(x)
@@ -114,16 +123,22 @@ yy = array(y)
 print ("Number of lines = ",linecount)
 # Locate Ymax, xmax, Ymin, and xmin:
 Ymax = max(y)
-xmax = y.index(Ymax)
+fymax = y.index(Ymax)
+Xmax = max(x)
+fxmax = x.index(Xmax)
 Ymin = min(y)
-xmin = y.index(Ymin)
+fymin = y.index(Ymin)
+Xmin = min(x)
+fxmin = x.index(Xmin)
 imax = len(x)
-print ("Maximum y = ",Ymax," Located at x = ",xmax)
-print ("Minimum y = ",Ymin," Located at x = ",xmin)
+print ("Maximum y = ",Ymax," Located at f = ",fymax)
+print ("Minimum y = ",Ymin," Located at x = ",fymin)
+print ("Maximum x = ",Xmax," Located at f = ",fxmax)
+print ("Minimum yx = ",Xmin," Located at x = ",fxmin)
 delx = (x[imax-1]-x[0])/10.0
 dely = (Ymax-Ymin)/20.0
 notex = 0.5*xx[imax-1]
-meanx = (xx[imax-1]+xx[0])/2.0
+meanx = (Xmax+Xmin)/2.0
 print ("delx = ",delx)
 print ("dely = ",dely)
 noteymax = 0.75*Ymax
@@ -135,10 +150,14 @@ loctime = time.asctime(time.localtime(time.time()))
 plt.plot(xx, yy, 'go', label='data')
 # Add appropriate error bars to represent experimental uncertainty:
 plt.errorbar(xx, yy, xerr=0.2, yerr=0.4)
-plt.annotate(r"$\mathscr{Y}_{max}$ = "+'{: 3.3e}'.format(Ymax),xy=(meanx,noteymax))
-plt.annotate(r"$\mathscr{x}_{max}$ = "+'{: 3.3e}'.format(xmax),xy=(meanx,noteymax-1.4*dely))
-plt.annotate(r"$\mathscr{Y}_{min}$ = "+'{: 3.3e}'.format(Ymin),xy=(meanx,noteymax-2.8*dely))
-plt.annotate(r"$\mathscr{x}_{min}$ = "+'{: 3.3e}'.format(xmin),xy=(meanx,noteymax-4.4*dely))
+plt.annotate(r"$\mathscr{y}_{max}$ = "+'{: 3.3e}'.format(Ymax),xy=(meanx+0.75*meanx,noteymax))
+plt.annotate(r"$\mathscr{f}_{ymax}$ = "+'{: 3.3e}'.format(fymax),xy=(meanx,noteymax))
+plt.annotate(r"$\mathscr{x}_{max}$ = "+'{: 3.3e}'.format(Xmax),xy=(meanx+0.75*meanx,noteymax-1.4*dely))
+plt.annotate(r"$\mathscr{f}_{ymax}$ = "+'{: 3.3e}'.format(fxmax),xy=(meanx,noteymax-1.4*dely))
+plt.annotate(r"$\mathscr{y}_{min}$ = "+'{: 3.3e}'.format(Ymin),xy=(meanx+0.75*meanx,noteymax-2.8*dely))
+plt.annotate(r"$\mathscr{f}_{ymin}$ = "+'{: 3.3e}'.format(Ymin),xy=(meanx,noteymax-2.8*dely))
+plt.annotate(r"$\mathscr{x}_{min}$ = "+'{: 3.3e}'.format(Xmin),xy=(meanx+0.75*meanx,noteymax-4.4*dely))
+plt.annotate(r"$\mathscr{f}_{xmin}$ = "+'{: 3.3e}'.format(fxmin),xy=(meanx,noteymax-4.4*dely))
 # Add date and time in plot title:
 loctime = time.asctime(time.localtime(time.time()))
 plt.suptitle('Data File ='+infile+':  '+loctime)
